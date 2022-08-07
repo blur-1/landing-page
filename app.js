843b84f6584153824ac8fff1fdd3783b73dbffff
blur-1 <blu.rac@gmail.com>
2022-08-06 20:54:15 -0500

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+function changeImage(newImg) {
+    document.querySelector('.banner').src =newImg;
+}
+
+function changeFondoColor(newColor){
+    document.querySelector('.circle-fondo').style.background= newColor;
+}
+
